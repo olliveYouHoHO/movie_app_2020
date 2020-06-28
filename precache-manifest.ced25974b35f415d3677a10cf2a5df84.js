9377b0c4dc452153146e16fcba199110743ba469
olliveYouHoHO <fpsk10547@naver.com>
2020-06-28 18:20:29 +0900

    Updates

---
+++ precache-manifest.ced25974b35f415d3677a10cf2a5df84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b29dcb63f188c2e0f69fa15232c4e6",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a342cfb17afe02f326bf",
+    "url": "/movie_app_2020/static/css/main.764345c0.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a342cfb17afe02f326bf",
+    "url": "/movie_app_2020/static/js/main.9b7b9b3b.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
